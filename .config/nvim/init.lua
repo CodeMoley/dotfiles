cd3195a56e68104a11f2964225ce62d9cf86591c
Joel Felton <joelfelton@pm.me>
2026-03-10 18:50:35 +0700

    added autopairs to neovim and some other small tweaks - need to fix tmux error

--- .config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require("config.lazy")
-require("config.keymaps")
-require("config.options")
-
---- SET COLORSCHEME ---
-vim.cmd("colorscheme gruvbox")
-
---- LSP CONFIG ---
-vim.lsp.enable({ "lua_ls", "rust_analyzer", "basedpyright", "gopls", "clangd", "zls" })
--- macos/.config/fish/config.fish
+++ macos/.config/fish/config.fish
@@ -8,6 +8,11 @@ alias cdd="cd ~/vault"
 # homebrew
 alias buu="brew update && brew upgrade"
 alias bs="brew search"
+alias bi="brew install"
+
+# odin
+alias odr="odin run"
+alias odb="odin build"
 
 # python
 alias py="python3"
@@ -16,9 +21,11 @@ alias venv="source .venv/bin/activate.fish"
 # neovim
 alias vim="nvim"
 alias vi="nvim"
+alias v="nvim"
 
 ### PATH VARIABLES ###
 fish_add_path ~/go/bin
+fish_add_path ~/vault/binaries/ols
 
 # launch starship
 starship init fish | source
